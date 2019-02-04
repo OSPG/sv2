e6e2d485797e001000153ca4ae6d282b6cc9587a
David Roman <davidroman96@gmail.com>
2019-02-04 19:21:30 +0100

    Closes: #15

--- sv2_checkers/ssh.py
+++ sv2_checkers/ssh.py
@@ -46,8 +46,7 @@ class _SSHConf(object):
 class SSHCheck(object):
 
     def __init__(self):
-        # XXX: In a near future we should add more default values for each one of the possible matches
-        with popen("/usr/sbin/sshd -T -C user=root 2>/dev/null") as p:
+        with popen("/usr/sbin/sshd -T -C \"user=root,addr=127.0.0.1,host=localhost,laddr=127.0.0.1,lport=22,rdomain=localhost\" 2>/dev/null") as p:
             sshd_config = p.read()
         self._sshd = _SSHConf(sshd_config)
 
